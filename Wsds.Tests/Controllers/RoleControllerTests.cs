df65cea2baa7369147f6dc417139139f5ed06072
Shenets-D <Shenets-D@NBKIECCO0054>
2018-05-04 15:05:37 +0300

    added new Tests of RoleController and SaveReviewController

--- Wsds.Tests/Controllers/RoleControllerTests.cs
+++ Wsds.Tests/Controllers/RoleControllerTests.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Wsds.WebApp.Models;
 
 namespace Wsds.Tests.Controllers
 {
@@ -35,8 +37,116 @@ namespace Wsds.Tests.Controllers
 
             var result = _roleController.AllRoles();
             Assert.IsType<OkObjectResult>(result);
+        }
 
-            //_mockIRoleRepository.Verify(x => x.AllRoles((role) => true),Times.Once);
+
+        [Theory]
+        [InlineData("admin")]
+        [InlineData("user")]
+        public void CreateRoleSuccessTest(string roleName)
+        {
+            _mockIRoleRepository.Setup(x => x.CreateRole(It.IsAny<string>())).Returns(CreateRole(roleName));
+            var result = _roleController.CreateRole(roleName);
+
+            Assert.IsType<Task<IActionResult>>(result);
+
+            Task<IActionResult> task = _roleController.CreateRole(roleName);
+            Assert.NotNull(task);
+
+            var taskIsCompleted = task.IsCompleted;
+            Assert.Equal(true, taskIsCompleted);
+
+            _mockIRoleRepository.Verify(x => x.CreateRole(roleName), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void CreateRoleFailTest()
+        {
+            _mockIRoleRepository.Setup(x => x.CreateRole(It.IsAny<string>())).Returns(CreateRole(""));
+            var result = _roleController.CreateRole(null);
+
+            Assert.IsType<Task<IActionResult>>(result);
+            Assert.Equal(true,result.IsCompleted);
+        }
+
+        [Theory]
+        [InlineData("10")]
+        [InlineData("11")]
+        public void DeleteRoleSuccessTest(string id)
+        {
+            _mockIRoleRepository.Setup(x => x.DeleteRole(It.IsAny<string>())).Returns(DeleteRole(id));
+            var result = _roleController.DeleteRole(id);
+
+            Assert.IsType<Task<IActionResult>>(result);
+
+            Task<IActionResult> task = _roleController.DeleteRole(id);
+            Assert.NotNull(task);
+
+            var taskIsCompleted = task.IsCompleted;
+            Assert.Equal(true, taskIsCompleted);
+
+            _mockIRoleRepository.Verify(x => x.DeleteRole(id), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void DeleteRoleFailTest()
+        {
+            _mockIRoleRepository.Setup(x => x.DeleteRole(It.IsAny<string>())).Returns(DeleteRole(""));
+            var result = _roleController.DeleteRole(null);
+
+            Assert.IsType<Task<IActionResult>>(result);
+            Assert.Equal(true, result.IsCompleted);
+        }
+
+        [Fact]
+        public void UpdateRoleSuccessTest()
+        {
+            var roleModel = new RoleShortModel() { Id="1",Name="name" };
+            _mockIRoleRepository.Setup(x => x.EditRole(It.IsAny<string>(), It.IsAny<string>())).Returns(EditRole(roleModel.Id,roleModel.Name));
+
+            var result = _roleController.UpdateRole(roleModel);
+
+            Assert.IsType<Task<IActionResult>>(result);
+
+            Task<IActionResult> task = _roleController.UpdateRole(roleModel);
+            Assert.NotNull(task);
+
+            var taskIsCompleted = task.IsCompleted;
+            Assert.Equal(true, taskIsCompleted);
+
+            _mockIRoleRepository.Verify(x => x.EditRole(roleModel.Id,roleModel.Name), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void UpdateRoleFailTest()
+        {
+            RoleShortModel model = null;
+            _roleController.ModelState.AddModelError("Id", "Required");
+
+            var result = _roleController.UpdateRole(model);
+
+            Assert.IsType<Task<IActionResult>>(result);
+            Assert.Equal(true, result.IsCompleted);
+        }
+
+        private Task<IdentityRole> CreateRole(string roleName)
+        {
+            return Task.FromResult<IdentityRole>(new IdentityRole(roleName));
+        }
+
+        private Task<BadRequestObjectResult> CreateRoleFailTest(string roleName)
+        {
+            return Task.FromResult<BadRequestObjectResult>(new BadRequestObjectResult("error"));
+        }
+
+        private Task<IdentityRole> DeleteRole(string id)
+        {
+            return Task.FromResult<IdentityRole>(new IdentityRole(id));
+        }
+
+        private Task<IdentityRole> EditRole(string id, string name)
+        {
+            return Task.FromResult<IdentityRole>(new IdentityRole(id));
         }
     }
 }
---
+++ Wsds.Tests/Controllers/SaveReviewControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+using Moq;
+using Wsds.DAL.Repository.Abstract;
+using Wsds.WebApp.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wsds.WebApp.Auth;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using Wsds.WebApp.WebExtensions;
+using Wsds.DAL.Entities;
+
+namespace Wsds.Tests.Controllers
+{
+    public class SaveReviewControllerTests
+    {
+        private Mock<IClientRepository> _mockIClientRepository;
+        private Mock<IReviewRepository> _mockIReviewRepository;
+        private SaveReviewController _saveReviewController;
+
+        public SaveReviewControllerTests()
+        {
+            _mockIClientRepository = new Mock<IClientRepository>();
+            _mockIReviewRepository = new Mock<IReviewRepository>();
+            _saveReviewController = new SaveReviewController(_mockIClientRepository.Object, _mockIReviewRepository.Object);
+        }
+
+        [Fact]
+        public void SaveReviewControllerCreated()
+        {
+            Assert.NotNull(_saveReviewController);
+        }
+
+        [Fact]
+        public void SaveStoreReviewSuccessTest()
+        {
+            _saveReviewController.ControllerContext = new ControllerContext();
+            _saveReviewController.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var token = new TokenModel() { Phone="380120000000",Card=1,ClientId=1};
+            var storeReview = new StoreReview_DTO() { id = 1, idClient = 1, user = "user" };
+            var client = new Client_DTO() { id = 1};
+
+            _mockIClientRepository.Setup(x => x.GetClientByPhone(It.IsAny<string>())).Returns(client);
+            _mockIReviewRepository.Setup(x => x.SaveStoreReview(It.IsAny<StoreReview_DTO>(), It.IsAny<Client_DTO>())).Returns(storeReview);
+            _saveReviewController.ControllerContext.HttpContext.Items["token"] = token;
+            
+            
+            var result = _saveReviewController.SaveStoreReview(storeReview);
+            Assert.IsType<CreatedAtRouteResult>(result);
+            Assert.NotNull(result);
+
+            _mockIClientRepository.Verify(x => x.GetClientByPhone(It.IsAny<string>()),Times.Once);
+            _mockIReviewRepository.Verify(x => x.SaveStoreReview(It.IsAny<StoreReview_DTO>(), It.IsAny<Client_DTO>()), Times.Once);    
+        }
+
+        [Fact]
+        public void SaveStoreReviewBadRequestTest()
+        {
+            _saveReviewController.ControllerContext = new ControllerContext();
+            _saveReviewController.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var storeReview = new StoreReview_DTO() { id = 1, idClient = 1, user = "user" };
+
+            var result = _saveReviewController.SaveStoreReview(storeReview);
+
+            Assert.IsType<BadRequestResult>(result);    
+        }
+    } 
+}
