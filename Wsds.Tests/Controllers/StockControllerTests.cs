3979d26b6e63bc743d96fd18a8072d20204c2f4d
Shenets-D <Shenets-D@NBKIECCO0054>
2018-05-04 16:43:26 +0300

    added the tests of the StockController

---
+++ Wsds.Tests/Controllers/StockControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Moq;
+using Wsds.DAL.Repository.Abstract;
+using Wsds.WebApp.Controllers;
+
+namespace Wsds.Tests.Controllers
+{
+    public class StockControllerTests
+    {
+        private Mock<IActionRepository> _mockIActionRepository;
+        private StockController _stockController;
+
+        public StockControllerTests()
+        {
+            _mockIActionRepository = new Mock<IActionRepository>();
+            _stockController = new StockController(_mockIActionRepository.Object);
+        }
+
+        [Fact]
+        public void StockControllerCreated()
+        {
+            Assert.NotNull(_stockController);
+        }
+    }
+}
