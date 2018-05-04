5daaac5e4eb75406890e5d00f164323db0703f2a
Shenets-D <Shenets-D@NBKIECCO0054>
2018-05-04 15:42:29 +0300

    there were changed tests of SaveReviewController

--- Wsds.Tests/Controllers/SaveReviewControllerTests.cs
+++ Wsds.Tests/Controllers/SaveReviewControllerTests.cs
@@ -5,13 +5,6 @@ using Wsds.WebApp.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Wsds.WebApp.Auth;
-using Microsoft.AspNetCore.Http.Authentication;
-using Microsoft.AspNetCore.Http.Features;
-using System;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading;
-using Wsds.WebApp.WebExtensions;
 using Wsds.DAL.Entities;
 
 namespace Wsds.Tests.Controllers
@@ -41,21 +34,21 @@ namespace Wsds.Tests.Controllers
             _saveReviewController.ControllerContext = new ControllerContext();
             _saveReviewController.ControllerContext.HttpContext = new DefaultHttpContext();
 
-            var token = new TokenModel() { Phone="380120000000",Card=1,ClientId=1};
+            var token = new TokenModel() { Phone = "380120000000", Card = 1, ClientId = 1 };
             var storeReview = new StoreReview_DTO() { id = 1, idClient = 1, user = "user" };
-            var client = new Client_DTO() { id = 1};
+            var client = new Client_DTO() { id = 1 };
 
             _mockIClientRepository.Setup(x => x.GetClientByPhone(It.IsAny<string>())).Returns(client);
             _mockIReviewRepository.Setup(x => x.SaveStoreReview(It.IsAny<StoreReview_DTO>(), It.IsAny<Client_DTO>())).Returns(storeReview);
             _saveReviewController.ControllerContext.HttpContext.Items["token"] = token;
-            
-            
+
+
             var result = _saveReviewController.SaveStoreReview(storeReview);
             Assert.IsType<CreatedAtRouteResult>(result);
             Assert.NotNull(result);
 
-            _mockIClientRepository.Verify(x => x.GetClientByPhone(It.IsAny<string>()),Times.Once);
-            _mockIReviewRepository.Verify(x => x.SaveStoreReview(It.IsAny<StoreReview_DTO>(), It.IsAny<Client_DTO>()), Times.Once);    
+            _mockIClientRepository.Verify(x => x.GetClientByPhone(It.IsAny<string>()), Times.Once);
+            _mockIReviewRepository.Verify(x => x.SaveStoreReview(It.IsAny<StoreReview_DTO>(), It.IsAny<Client_DTO>()), Times.Once);
         }
 
         [Fact]
@@ -65,10 +58,52 @@ namespace Wsds.Tests.Controllers
             _saveReviewController.ControllerContext.HttpContext = new DefaultHttpContext();
 
             var storeReview = new StoreReview_DTO() { id = 1, idClient = 1, user = "user" };
+            var token = new TokenModel() { Phone = "380120000000", Card = 1, ClientId = 1 };
 
             var result = _saveReviewController.SaveStoreReview(storeReview);
 
-            Assert.IsType<BadRequestResult>(result);    
+            Assert.IsType<BadRequestResult>(result);
+
+            _mockIClientRepository.Verify(x => x.GetClientByPhone(It.IsAny<string>()), Times.Never);
+            _mockIReviewRepository.Verify(x => x.SaveStoreReview(It.IsAny<StoreReview_DTO>(), It.IsAny<Client_DTO>()), Times.Never);
         }
-    } 
+
+        [Fact]
+        public void SaveProductReviewTest()
+        {
+            var client = new Client_DTO() { id = 1 };
+            var product = new ProductReview_DTO() { id = 1 };
+
+            _mockIClientRepository.Setup(x => x.GetClientByPhone(It.IsAny<string>())).Returns(client);
+            _mockIReviewRepository.Setup(x => x.SaveProductReview(It.IsAny<ProductReview_DTO>(), It.IsAny<Client_DTO>())).Returns(product);
+
+            var token = new TokenModel() { Phone = "380120000000", Card = 1, ClientId = 1 };
+            _saveReviewController.ControllerContext = new ControllerContext();
+            _saveReviewController.ControllerContext.HttpContext = new DefaultHttpContext();
+            _saveReviewController.ControllerContext.HttpContext.Items["token"] = token;
+
+            var result = _saveReviewController.SaveProductReview(product);
+            Assert.IsType<CreatedAtRouteResult>(result);
+
+            _mockIClientRepository.Verify(x => x.GetClientByPhone(It.IsAny<string>()), Times.Once);
+            _mockIReviewRepository.Verify(x => x.SaveProductReview(It.IsAny<ProductReview_DTO>(), It.IsAny<Client_DTO>()), Times.Once);
+        }
+
+        [Fact]
+        public void SaveProductReviewBadRequestTest()
+        {
+            _saveReviewController.ControllerContext = new ControllerContext();
+            _saveReviewController.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var product = new ProductReview_DTO() { id = 1 };
+            var token = new TokenModel() { Phone = "380120000000", Card = 1, ClientId = 1 };
+
+            var result = _saveReviewController.SaveProductReview(product);
+
+            Assert.IsType<BadRequestResult>(result);
+
+            _mockIClientRepository.Verify(x => x.GetClientByPhone(It.IsAny<string>()), Times.Never);
+            _mockIReviewRepository.Verify(x => x.SaveProductReview(It.IsAny<ProductReview_DTO>(), It.IsAny<Client_DTO>()), Times.Never);
+        }
+    }
 }
